5d2ad6f065005c2a541b8eb9cf801388114275f7
tomasfv <tomasfernandezvaldes@gmail.com>
2023-05-22 14:19:57 -0300

    fix cv download

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -58,7 +58,10 @@ export const Banner = () => {
                 <span className="tagline">Welcome to my Portfolio</span>
                 <h1>{`Hi! I'm Tomás:`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Software Developer.", "Game Developer.", "Technology Lover." ]'><span className="wrap">{text}</span></span></h1>
                   <p>Full Stack and Game Developer with experience in C#, Unity, Wwise, Javascript, React, React Native, HTML, CSS, Node.js, Sequelize, and PostgreSQL, among other technologies.</p>
-                  <a download href="./TomásFernándezValdés_SoftwareDev_ENG.pdf">Download CV <ArrowRightCircle size={25} /></a>
+                  {/* <button><a download href="/TomásFernándezValdés_SoftwareDev_ENG.pdf">Download CV <ArrowRightCircle size={25} /></a></button> */}
+                  <a download href="./TomásFernándezValdés_ SoftwareDev_ENG.pdf">
+                    <button>Download CV</button>
+                  </a>
               </div>}
             </TrackVisibility>
           </Col>
