4cdb6f2d2f087bec24bed0d2715e30abf4844642
tomasfv <tomasfernandezvaldes@gmail.com>
2023-05-22 14:48:19 -0300

    mod cv

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -59,7 +59,7 @@ export const Banner = () => {
                 <h1>{`Hi! I'm Tomás:`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Software Developer.", "Game Developer.", "Technology Lover." ]'><span className="wrap">{text}</span></span></h1>
                   <p>Full Stack and Game Developer with experience in C#, Unity, Wwise, Javascript, React, React Native, HTML, CSS, Node.js, Sequelize, and PostgreSQL, among other technologies.</p>
                   {/* <button><a download href="/TomásFernándezValdés_SoftwareDev_ENG.pdf">Download CV <ArrowRightCircle size={25} /></a></button> */}
-                  <a download="../assets/pdf/TomásFernándezValdés_SoftwareDev_ENG.pdf" href="../assets/pdf/TomásFernándezValdés_SoftwareDev_ENG.pdf">
+                  <a href="https://github.com/tomasfv/SoftwareDevPortfolio/blob/main/src/assets/pdf/Tom%C3%A1sFern%C3%A1ndezVald%C3%A9s_SoftwareDev_ENG.pdf">
                     <button>Download CV</button>
                   </a>
               </div>}
